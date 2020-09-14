bda69fb7bbeb6ec74872a6b0a8c69c058034d597
Christopher Sorta <chris.sorta@yahoo.com>
2020-09-14 14:14:55 -0700

    Implement keeping track of the previous winner

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,74 +1,92 @@
 
-// Variables to track state
+// Variables to track state/model
 
-const winningCombo = [['1','4','7'], ['2','5','8'], ['3','6','9'], ['1','2','3'], ['4','5','6'], ['7','8','9']
-, ['1','5','9'], ['3','5','7']];
+const winningCombo = [['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'], ['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']
+  , ['1', '5', '9'], ['3', '5', '7']];
 let moves = 0;
 
-var currentPlayer = 'X';
+let currentPlayer = 'X';
+let recentWinner = '' || 'X';
 let xBank = [];
 let oBank = [];
+let xTotalWins = 0;
+let oTotalWins = 0;
 
-//Event Handlers -- controllers
+//Event Handlers
 let board = document.getElementById('board');
-board.addEventListener('click', function(e){
+board.addEventListener('click', function (e) {
 
-  if(isLegalMove(e.target.id.toString())) {
-    el = document.getElementById(e.target.id);
-    el.innerHTML = currentPlayer;
-  moves++;
-  addScore(currentPlayer, e.target.id);
-  changePlayer();
-  checkWinner();
-}
+  if (isLegalMove(e.target.id.toString())) {
+    placePiece(e);
+    addScore(currentPlayer, e.target.id);
+    changePlayer();
+    checkWinner();
+  }
 
 })
 
 button = document.getElementById('button');
-button.addEventListener('click', function() {
-  for(let i = 1; i <= 9; i++) {
-    document.getElementById(i.toString()).innerHTML = '';
-  }
+button.addEventListener('click', function () {
   resetBoard();
 
 })
 
-// Functions to change game state
-var arrayMatch = (arr1, winningArray) => {
- for(let i = 0; i < winningArray.length; i++) {
-   if(arr1.indexOf(winningArray[i]) === -1) {
-     return false;
-   }
- }
- return true;
+//########################################
+// Functions to change view
+
+// Place piece on the board
+var placePiece = (e) => {
+  el = document.getElementById(e.target.id);
+  el.innerHTML = currentPlayer;
 }
 
-var checkWinner = () => {
 
-  for(let i = 0; i < winningCombo.length; i++) {
-    if(arrayMatch(xBank, winningCombo[i])) {
-      console.log('hi');
-      alert('X won the game');
-    }
-    if(arrayMatch(oBank, winningCombo[i])) {
-      alert('O won the game');
+//########################################
+
+// Functions to change game state -- controllers
+
+// check if a player matches to one winning combo
+var arrayMatch = (arr1, winningArray) => {
+  for (let i = 0; i < winningArray.length; i++) {
+    if (arr1.indexOf(winningArray[i]) === -1) {
+      return false;
     }
   }
-  if(moves === 9) {
+  return true;
+}
+
+// check the players against all possible winning combinations
+var checkWinner = () => {
+  for (let i = 0; i < winningCombo.length; i++) {
+    if (arrayMatch(xBank, winningCombo[i])) {
+      alert('X won the game');
+      recentWinner = 'X';
+      xTotalWins++;
+    }
+    if (arrayMatch(oBank, winningCombo[i])) {
+      alert('O won the game');
+      recentWinner = 'O';
+      oTotalWins++;
+
+    }
+  }
+  if (moves === 9) {
     alert("Tied Game");
   }
 }
 
+// Checks if there is already an X or O on the board
 var isLegalMove = (value) => {
-  if(xBank.indexOf(value) !== -1 || oBank.indexOf(value) !== -1) {
+  if (xBank.indexOf(value) !== -1 || oBank.indexOf(value) !== -1) {
     return false;
   }
   return true;
 }
 
+// Change who the current player is
 var changePlayer = () => {
 
-  if(currentPlayer === 'X') {
+  if (currentPlayer === 'X') {
     currentPlayer = 'O';
     return 'O';
   } else {
@@ -77,21 +95,26 @@ var changePlayer = () => {
   }
 }
 
+// Track where the player placed their mark
 var addScore = (player, value) => {
-  if(player === 'X') {
+  if (player === 'X') {
     xBank.push(value);
     xBank = xBank.sort();
   } else {
     oBank.push(value);
     oBank = oBank.sort();
   }
+  moves++;
 }
 
+// Reset state
 var resetBoard = () => {
-
+  for (let i = 1; i <= 9; i++) {
+    document.getElementById(i.toString()).innerHTML = '';
+  }
   xBank = [];
   oBank = [];
-  currentPlayer = 'X';
+  currentPlayer = recentWinner;
   moves = 0;
 }
 
